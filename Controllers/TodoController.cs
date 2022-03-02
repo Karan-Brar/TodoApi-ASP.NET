9358af6d654e2ebb3f5f14714b62559cce0827be
Karan <brarkaran938@gmail.com>
2022-03-02 14:50:36 -0600

    Initial commit

---
+++ Controllers/TodoController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TodoApi.Services;
+using TodoApi.Models;
+
+namespace TodoApi.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class TodoController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<List<TodoItem>> GetAll() =>
+            TodoItemService.GetAll();
+
+        [HttpGet("{id}")]
+        public ActionResult<TodoItem> Get(int id)
+        {
+            var todoItem = TodoItemService.Get(id);
+
+            if(todoItem == null)
+            {
+                return NotFound();
+            }
+
+            return todoItem;
+        }
+
+
+
+    }
+}
