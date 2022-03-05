f9536fe29321c97c2eef964b4f834e66625484e3
Karan <brarkaran938@gmail.com>
2022-03-05 13:22:15 -0600

    Remove the now ignored files and added some docs to the controller

--- Controllers/TodoController.cs
+++ Controllers/TodoController.cs
@@ -4,29 +4,45 @@ using TodoApi.Models;
 
 namespace TodoApi.Controllers
 {
+    /// <summary>
+    /// This is the main controller class that will map to the route ending with "/Todo".
+    /// </summary>
     [Route("[controller]")]
     [ApiController]
     public class TodoController : ControllerBase
     {
         private readonly TodoContext _context;
 
+        /// <summary>
+        /// Initializes an instance of the controller class.
+        /// </summary>
+        /// <param name="context">An instance of TodoContext that will be used by the controller</param>
         public TodoController(TodoContext context)
         {
             _context = context;
 
             if (_context.TodoItems.Count() == 0)
             {
-                _context.TodoItems.Add(new TodoItem { Description = "blah blah" });
+                _context.TodoItems.Add(new TodoItem { Description = "Buy movie tickets" });
                 _context.SaveChanges();
             }
         }
 
+        /// <summary>
+        /// Responds to GET requests at the route "/Todo".
+        /// </summary>
+        /// <returns>The List of to-do items in the database.</returns>
         [HttpGet]
         public ActionResult<List<TodoItem>> GetAll()
         {
             return _context.TodoItems.ToList();
         }
 
+        /// <summary>
+        /// Responds to GET requests at the route "/Todo/{id}" where {id} is the ID of the To-do item to be retrieved.
+        /// </summary>
+        /// <param name="id">The ID of the to-do item passed as a parameter in the URL.</param>
+        /// <returns>A specific action result or the to-do item found.</returns>
         [HttpGet("{id}")]
         public ActionResult<TodoItem> Get(int id)
         {
@@ -40,7 +56,11 @@ namespace TodoApi.Controllers
             return todoItem;
         }
 
-
+        /// <summary>
+        /// Responds to the POST requests at the route "/Todo".
+        /// </summary>
+        /// <param name="todoItem">The to-do item data from the request body.</param>
+        /// <returns>A specific action result.</returns>
         [HttpPost]
         public IActionResult Create(TodoItem todoItem)
         {
@@ -49,6 +69,12 @@ namespace TodoApi.Controllers
             return CreatedAtAction(nameof(Create), new { id = todoItem.Id }, todoItem);
         }
 
+        /// <summary>
+        /// Responds to PUT requests at the route "/Todo/{id}" where {id} is the ID of the To-do item to be updated.
+        /// </summary>
+        /// <param name="id">The ID of the to-do item passed as a parameter in the URL.</param>
+        /// <param name="todoItem">The to-do item data from the request body.</param>
+        /// <returns>A specific action result.</returns>
         [HttpPut("{id}")]
         public IActionResult Update(int id, TodoItem todoItem)
         {
@@ -63,15 +89,24 @@ namespace TodoApi.Controllers
                 return NotFound();
             }
 
-            existingTodoItem.Description = todoItem.Description;
+            if(todoItem.Description != null)
+            {
+                existingTodoItem.Description = todoItem.Description;
+            }
+
             existingTodoItem.IsComplete = todoItem.IsComplete;
+            
 
             _context.SaveChanges();
 
             return NoContent();
         }
 
-
+        /// <summary>
+        /// Responds to DELETE requests at the route "/Todo/{id}" where {id} is the ID of the To-do item to be deleted.
+        /// </summary>
+        /// <param name="id">The ID of the to-do item passed as a parameter in the URL</param>
+        /// <returns>A specific action result.</returns>
         [HttpDelete("{id}")]
         public IActionResult Delete(int id)
         {
